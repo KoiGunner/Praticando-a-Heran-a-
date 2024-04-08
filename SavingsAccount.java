6e6fef2582843835a1083955113f093a3e9303da
Aleksandry Cesar <57228025+KoiGunner@users.noreply.github.com>
2024-04-08 15:08:12 -0300

    Create SavingsAccount.java
    Aplicando a extensão da classe Account nessa nova, implementando um novo atributo chamado rate. Ele será complementado com diversas outras funções

---
+++ SavingsAccount.java
@@ -0,0 +1,13 @@
+public class SavingsAccount extends Account{
+  private double rate;
+
+  public getRate(){
+    return rate;
+  }
+
+  public void setRate(double rate){
+    this.rate = rate;
+  }
+
+  
+}
